02b68234071cd5d05fac72cc039613232566d513
Abhishek <asanthakumar@albany.edu>
2026-04-17 11:26:56 -0400

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 // Allow localhost in dev and the real frontend URL in production
 const allowedOrigins = [
+  'https://portfolio-1-bosg.onrender.com'
   'http://localhost:5173',
   'http://localhost:4173', // vite preview
   process.env.FRONTEND_URL,  // set this in production e.g. https://yoursite.onrender.com
